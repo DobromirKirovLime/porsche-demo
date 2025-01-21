f759e2f86bc191297037f90ea73ffd0ece284a7b
Dobromir Kirov <dobromir.kirov@limechain.tech>
2025-01-21 13:03:41 +0200

    feat: cretaed contentful client

---
+++ src/services/contentful.ts
@@ -0,0 +1,8 @@
+import { createContentClient } from "@/config/contentful";
+
+const client = createContentClient();
+
+export const getEntriesByType = async (type: string) => {
+  const response = await client.getEntries({ content_type: type });
+  return response.items;
+};
